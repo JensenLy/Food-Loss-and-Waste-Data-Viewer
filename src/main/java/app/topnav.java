27317ace70f93852f1efda798c11c4aff06b7626
Kien Hung (Jensen) Ly <s3977367@student.rmit.edu.au>
2024-05-27 10:56:07 +1000

    removed "current" class to avoid duplication

--- src/main/java/app/topnav.java
+++ src/main/java/app/topnav.java
@@ -8,7 +8,7 @@ public class topnav {
             </div>
 
             <div class = "navbar">
-                <a class = "current" href="/">Home</a>
+                <a href="/">Home</a>
                 <a href="/page2A.html">Surface Data</a>
                 <a href="/page3A.html">In-depth Data</a>
                 <a href="/mission.html">About Us</a>
