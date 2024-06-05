4f999ecf7b3debb9252d84078ea9934a7aa9392e
Kien Hung (Jensen) Ly <s3977367@student.rmit.edu.au>
2024-06-05 22:04:35 +1000

    personaAndStudent.java for 1B

---
+++ src/main/java/app/PersonaAndStudent.java
@@ -0,0 +1,56 @@
+package app;
+
+/**
+ * Class represeting a Country from the Studio Project database
+ *
+ * @author Halil Ali, 2024. email: halil.ali@rmit.edu.au
+ */
+
+public class PersonaAndStudent {
+   public String studentName;
+
+   public String studentID;
+
+   public String studentEmail;
+
+   public String personaName;
+   
+   public String personaImagePath;
+   
+   public int personaAge;
+
+   public String personaGender;
+
+   public String personaEthnicity;
+
+   public String personaBackground;
+
+   public String personaNeed;
+
+   public String personaSkill;
+
+   public PersonaAndStudent(){
+   }
+
+   public PersonaAndStudent(String studentName, String studentID, String studentEmail, String personaName, String personaImagePath, int personaAge, String personaGender, String personaEthnicity, String personaBackground, String personaNeed, String personaSkill) {
+      this.studentName = studentName;
+      this.studentID = studentID;
+      this.studentEmail = studentEmail;
+      this.personaName = personaName;
+      this.personaImagePath = personaImagePath;
+      this.personaAge = personaAge;
+      this.personaGender = personaGender;
+      this.personaEthnicity = personaEthnicity;
+      this.personaBackground = personaBackground;
+      this.personaNeed = personaNeed;
+      this.personaSkill = personaSkill;
+   }
+
+   // public String getName() {
+   //    return name;
+   // }
+
+   // public String getPercentage() {
+   //    return percentage;
+   // }
+}
