238ef83e4260f60dc1fb27af672b108508061071
Kien Hung (Jensen) Ly <s3977367@student.rmit.edu.au>
2024-06-18 22:51:33 +1000

    separate the radio buttons

--- src/main/java/app/PageST2B.java
+++ src/main/java/app/PageST2B.java
@@ -87,7 +87,7 @@ public class PageST2B implements Handler {
         html = html + outputFoodGroups();
 
         html = html + """ 
-        <h2> ---------------------------------------------</h2>
+        <h2> -----------------------------------------</h2>
         
         <h2> Enter Year</h2>
 
@@ -96,7 +96,7 @@ public class PageST2B implements Handler {
         <input type = "number" placeholder="End Year" min = "1966" max = "2022" name="EndYear" autofocus required>
         </div>
 
-        <h2> ---------------------------------------------</h2>
+        <h2> -----------------------------------------</h2>
         
         <h2>Filter options</h2>
 
@@ -115,12 +115,17 @@ public class PageST2B implements Handler {
         <label for="check3">Cause of food loss/waste</label>
         </div>
 
-        <h2> ---------------------------------------------</h2>
+        <h2> -----------------------------------------</h2>
 
         <h2>Sort loss percentage</h2>
 
-        <label><input type="radio" name="sort" value = "Ascending">Ascending</label>
+        <div>
+        <label><input type="radio" name="sort" value = "Ascending" checked>Ascending</label>
+        </div>
+
+        <div>
         <label><input type="radio" name="sort" value = "Descending">Descending</label>
+        </div>
 
         <input type = "submit" value = "Search">
 
